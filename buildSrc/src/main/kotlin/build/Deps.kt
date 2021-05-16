353a5197b385047a5f03c5609b429c00017171e9
Xiaodong Wang <xiaodongw79@gmail.com>
2021-05-16 16:03:40 -0700

    Upgrade coroutine to 1.5.0.

--- buildSrc/src/main/kotlin/build/Deps.kt
+++ buildSrc/src/main/kotlin/build/Deps.kt
@@ -3,9 +3,9 @@ package build
 @Suppress("unused")
 object Deps {
   const val kotlinVersion = "1.5.0"
-  const val kotlinCoroutineVersion = "1.5.0-RC"
+  const val kotlinCoroutineVersion = "1.5.0"
   const val protobufVersion = "3.15.8"
-  const val grpcJavaVersion = "1.37.0"
+  const val grpcJavaVersion = "1.37.1"
   const val kotestVersion = "4.5.0"
   const val vertxVersion = "4.0.3"
 }
