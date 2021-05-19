528ee4de2205df2312cf93b47eafa42bbb96782d
Xiaodong Wang <xiaodongw79@gmail.com>
2021-05-18 22:29:28 -0700

    Upgrade protobuf to 3.17.0, grpc-java to 1.38.0, kotest to 4.6.0.

--- buildSrc/src/main/kotlin/build/Deps.kt
+++ buildSrc/src/main/kotlin/build/Deps.kt
@@ -4,8 +4,8 @@ package build
 object Deps {
   const val kotlinVersion = "1.5.0"
   const val kotlinCoroutineVersion = "1.5.0"
-  const val protobufVersion = "3.15.8"
-  const val grpcJavaVersion = "1.37.1"
-  const val kotestVersion = "4.5.0"
+  const val protobufVersion = "3.17.0"
+  const val grpcJavaVersion = "1.38.0"
+  const val kotestVersion = "4.6.0"
   const val vertxVersion = "4.0.3"
 }
