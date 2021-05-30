3f9f777e229fcb0787e7ddb13edd07747b1dfc69
Xiaodong Wang <xiaodongw79@gmail.com>
2021-05-30 11:11:56 -0700

    Add github action to run tests.

--- kert-compiler/build.gradle.kts
+++ kert-compiler/build.gradle.kts
@@ -8,3 +8,16 @@ dependencies {
   testImplementation(project(":kert-grpc"))
   testImplementation("com.google.protobuf:protobuf-kotlin:${Deps.protobufVersion}")
 }
+
+tasks {
+  register("setup") {
+    exec {
+      commandLine = listOf(
+        "go", "get", "-t",
+        "github.com/golang/protobuf/proto",
+        "github.com/golang/protobuf/protoc-gen-go/descriptor",
+        "github.com/golang/protobuf/protoc-gen-go/plugin"
+      )
+    }
+  }
+}
