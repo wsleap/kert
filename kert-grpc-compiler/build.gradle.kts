a49259b755810e9807d589fd0d991b52c0d1e52f
Xiaodong Wang <xiaodongw79@gmail.com>
2023-04-29 20:35:42 -0700

    build: fix github action build.

--- kert-grpc-compiler/build.gradle.kts
+++ kert-grpc-compiler/build.gradle.kts
@@ -8,16 +8,3 @@ dependencies {
   testImplementation(project(":kert-grpc"))
   testImplementation(libs.protobuf.kotlin)
 }
-
-tasks {
-  /*register("setup") {
-    exec {
-      commandLine = listOf(
-        "go", "get", "-t",
-        "github.com/golang/protobuf/proto",
-        "github.com/golang/protobuf/protoc-gen-go/descriptor",
-        "github.com/golang/protobuf/protoc-gen-go/plugin"
-      )
-    }
-  }*/
-}
